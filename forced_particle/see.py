1a804883f73d0f99e736955525898d4a8f6b3fd7
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-05-26 10:50:32 +0200

    display a regular matplotlib plot of x,y,z (driven) and z for all

--- forced_particle/see.py
+++ forced_particle/see.py
@@ -1,19 +1,17 @@
 import numpy as np
 import h5py
-import mayavi.mlab as mlab
+import matplotlib.pyplot as plt
 
 a = h5py.File('dump_prism.h5', 'r')
 
 obstacle = a['/particles/obstacle/position'][:]
 pos = a['/particles/colloids/position/value']
 
-x = pos[0,:,0]
-y = pos[0,:,1]
-z = pos[0,:,2]
+ax1 = plt.subplot(211)
+plt.plot(pos[:,0,:])
+ax2 = plt.subplot(212, sharex=ax1)
+plt.plot(pos[:,:,2])
 
 a.close()
 
-mlab.points3d(obstacle[:,0], obstacle[:,1], obstacle[:,2], scale_factor=2, color=(1, 0, 0))
-mlab.points3d(x,y,z, scale_factor=2, color=(0, 0, 1))
-
-mlab.show()
+plt.show()
