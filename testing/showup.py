3875bf08a0f3fad97c23401671a9830e3b9105a3
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-06-18 14:36:00 +0200

    take data filename as input and label vertices

--- testing/showup.py
+++ testing/showup.py
@@ -2,12 +2,15 @@ from __future__ import print_function, division
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D                                                                                                                                   
+import sys
 
-i, x, y, z = np.loadtxt('i_p.txt', unpack=True)
+data = np.loadtxt(sys.argv[1])
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.plot(x, y, z)
+ax.plot(*zip(*data[:,1:]))
+for i, x, y, z in data:
+    ax.text(x, y, z, str(int(i)))
 
 plt.show()
