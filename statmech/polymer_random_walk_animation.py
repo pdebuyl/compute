fdaf46c57b17f3b1d2c542b25e8485da41b115d2
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-10-05 11:44:14 +0200

    add animation of a random polymer

---
+++ statmech/polymer_random_walk_animation.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy import pi
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+plt.rcParams['font.size'] = 18
+plt.rcParams['axes.labelsize'] = 'large'
+plt.rcParams['figure.subplot.left'] = 0.15
+plt.rcParams['figure.subplot.bottom'] = 0.15
+
+N = 10
+
+angles = np.random.random_sample(N)*2*pi
+
+f = plt.figure()
+plt.grid()
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
+
+def x_y_from_angles(angles):
+    bonds = np.zeros((N,2))
+    bonds[:,0] = np.cos(angles)
+    bonds[:,1] = np.sin(angles)
+    x = np.concatenate(((0,),np.cumsum(np.cos(angles))))
+    y = np.concatenate(((0,),np.cumsum(np.sin(angles))))
+    return x,y
+
+artists = []
+
+def init():
+    l, = plt.plot(*x_y_from_angles(angles),marker='o',markersize=20)
+    artists.append(l)
+    plt.xlim(-5,5); plt.ylim(-5,5);
+    return artists
+
+def anim_f(i):
+    global angles
+    angles = angles + np.random.normal(0, 0.1, N)
+    artists[0].set_data(*x_y_from_angles(angles))
+    return artists
+
+ani = animation.FuncAnimation(f, anim_f, frames=10, init_func=init, interval=200)
+
+plt.show()
