7a1c601a487b53c54c21654f62ac193fe253c91e
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-10-23 09:44:22 +0200

    reorder argument declaration

--- brownian_dynamics/fortran/brownian_wrapper.f90
+++ brownian_dynamics/fortran/brownian_wrapper.f90
@@ -17,16 +17,16 @@ contains
        lambda, sigma, cut, &
        rot_eps, force_type, &
        data, probe_data, dim, n_bath, force, force_count) bind(c)
-    integer(c_int), intent(in) :: n_bath
     real(c_double), intent(in) :: x0(dim, n_bath)
     real(c_double), intent(in) :: probe_x0(dim)
     real(c_double), intent(in) :: D, probe_D, dt, origin_k, origin_sigma, &
          wall_k, wall_sigma, probe_wall_k, probe_wall_sigma, lambda, sigma, cut, rot_eps
-    integer(c_int), intent(in) :: dim
     integer(c_int), intent(in) :: nloop, nsteps, nskip
     integer(c_int), intent(in) :: force_type
     real(c_double), intent(out) :: data(dim, n_bath, nsteps), &
          probe_data(dim, nsteps)
+    integer(c_int), intent(in) :: dim
+    integer(c_int), intent(in) :: n_bath
     real(c_double), intent(out) :: force(nbins)
     integer(c_int), intent(out) :: force_count(nbins)
 
