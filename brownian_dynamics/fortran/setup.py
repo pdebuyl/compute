76feb2b65965ec6250254d04c9969cd2c7aa37f5
Pierre de Buyl <pdebuyl@pdebuyl.be>
2015-10-30 12:59:36 +0100

    remove unused variable

--- brownian_dynamics/fortran/setup.py
+++ brownian_dynamics/fortran/setup.py
@@ -7,8 +7,6 @@ import os.path
 here = os.path.dirname(os.path.abspath(__file__))
 obj_dir = 'build'
 
-fortran_objects = [ os.path.join(here,obj_dir,o_name) for o_name in [ "brownian.o", "brownian_wrapper.o", "mtprng.o", "stdtypes.o" ]]
-
 ext_modules = [
     Extension("brownian_wrapper",
               ["brownian_wrapper.pyx"],
