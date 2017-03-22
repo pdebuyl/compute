2e0731c5fc575e629a08c3b8396aaafd6e8fcc4e
Pierre de Buyl <pdebuyl@pdebuyl.be>
2017-03-22 10:32:01 +0100

    remove dead code and pep8 a bit

--- janus_2017/colloids_to_janus.py
+++ janus_2017/colloids_to_janus.py
@@ -8,6 +8,11 @@ The resulting clusters are output to numbered H5MD files containing the
 """
 from __future__ import print_function, division
 import argparse
+import pyh5md
+import numpy as np
+import itertools
+import os.path
+from transforms3d import quaternions
 
 
 def parse_args(args=None):
@@ -23,12 +28,6 @@ def parse_args(args=None):
     parser.add_argument('--group', type=str, default='colloids',
                         help='name of the particles group')
     return parser.parse_args(args)
-
-import pyh5md
-import numpy as np
-import itertools
-import os.path
-from transforms3d import quaternions
 
 
 def center_cluster(r, edges):
@@ -61,14 +60,6 @@ def inertia_gyr(r, mass_bead):
         I[2] += pos[0]**2 + pos[1]**2
     return I*mass_bead, gyr
 
-def full_inertia(r, mass_bead):
-    I = np.zeros((3,3))
-    I[0,0], I[1,1], I[2,2] = -np.sum(r**2, axis=0)
-    for part in range(r.shape[0]):
-        pos = r[part, :]
-        I += np.outer(pos, pos)
-    return I*mass_bead
-
 
 def transform(r, target_Iz):
     x, y, z = np.sum(r**2, axis=0)
@@ -76,29 +67,6 @@ def transform(r, target_Iz):
         r[part, 0] *= np.sqrt(z/x)
         r[part, 1] *= np.sqrt(z/y)
 
-
-def rotate_z(x, theta):
-    return np.dot(np.array([[np.cos(theta), -np.sin(theta), 0],
-                            [np.sin(theta), np.cos(theta), 0],
-                            [0, 0, 1]]), x)
-
-
-def rotate_y(x, theta):
-    return np.dot(np.array([[np.cos(theta), 0, -np.sin(theta)],
-                            [0, 1, 0],
-                            [np.sin(theta), 0, np.cos(theta)]]), x)
-
-
-def align_to_z(r, v):
-    theta = np.arctan2(v[1], v[0])
-    for i in range(r.shape[0]):
-        r[i, :] = rotate_z(r[i, :], -theta)
-    v = rotate_z(v, -theta)
-    phi = np.arctan(v[2]/v[0])
-    for i in range(r.shape[0]):
-        r[i, :] = rotate_y(r[i, :], np.pi/2-phi)
-    v = rotate_y(v, np.pi/2-phi)
-    return r
 
 def inertia_tensor(r):
     r = np.asarray(r)
@@ -108,6 +76,7 @@ def inertia_tensor(r):
         res -= np.outer(r[i], r[i])
 
     return res
+
 
 def align_axis(r, idx):
     r = np.asarray(r)
@@ -124,12 +93,14 @@ def align_axis(r, idx):
         r[i] = quaternions.rotate_vector(r[i], q)
     return r
 
+
 def balance_diagonal(r):
     x, y, z = inertia_tensor(r).diagonal()
     r[:,0] /= np.sqrt((y+z-x))
     r[:,1] /= np.sqrt((x+z-y))
     r[:,2] /= np.sqrt((x+y-z))
     return r
+
 
 def write_configuration(r, species, filename, sigma, treshold):
     kwargs = {'author': 'Pierre de Buyl',
@@ -146,47 +117,16 @@ def write_configuration(r, species, filename, sigma, treshold):
         pyh5md.element(cluster, 'species', store='fixed', data=species)
 
 
-def cut_in_half(r):
-    """Sort the particles so that the first and second half represent the two
-    hemispheres of the sphere. Assumes that the particles are centered around
-    the origin."""
-    assert np.allclose(r.mean(axis=0), np.zeros(r.shape[1]))
-    n = r.shape[0]
-    is_odd = not n%2==0
-    weigth = np.inf
-    keep_up = None
-    for i in range(n):
-        v = r[i] / np.sqrt(np.sum(r[i]**2))
-        sign = np.sum(r*v.reshape((1, 3)), axis=1)
-        up = sign > 0
-        r_up = r[up]
-        r_down = r[~up]
-        if len(r_up) == n//2 or (is_odd and len(r_up) == n//2+1):
-            new_weigth = np.sum(sign**2)
-            if new_weigth < weigth:
-                keep_i = i
-                weigth = new_weigth
-                keep_up = up.copy()
-                print(args.file, weigth)
-    i, up = keep_i, keep_up
-    r_up = r[up]
-    r_down = r[~up]
-    new_index = np.searchsorted(np.arange(n)[up], i)
-    assert np.allclose(r[i], r_up[new_index])
-    r = np.concatenate((r_up, r_down))
-    species = np.zeros(n)
-    species[:] = 2
-    species[:np.sum(up)] = 1
-    return r, species, new_index
-
 def cut_along_xyz(r):
     assert np.allclose(r.mean(axis=0), np.zeros(r.shape[1]))
     n_up = np.sum(r>0, axis=0)
     n_down = np.sum(r<0, axis=0)
     return np.equal(n_up, n_down)
 
+
 def change_axis(r, new_z):
     return r[:,np.roll(np.arange(3), 2-new_z)]
+
 
 # read
 # select clusters
@@ -235,5 +175,6 @@ if __name__ == '__main__':
             species = np.ones(r.shape[0])
             species[r[:,2]<0] = 2
             filename = '%s_janus_b_%03i.h5' % (args.file[:-3], i+1)
-            write_configuration(r, species, filename, args.sigma*args.scale, args.treshold*args.scale)
+            write_configuration(r, species, filename, args.sigma*args.scale,
+                                args.treshold*args.scale)
             print('Wrote', filename)
